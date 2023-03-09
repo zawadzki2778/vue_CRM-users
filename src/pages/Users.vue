3d3df50d5156be87b51905e3f23cf9199d67d8b1
Maksim Zawadzki <zawadzki2778@gmail.com>
2023-03-09 15:35:21 +0300

    test

---
+++ src/pages/Users.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="wrapper-content wrapper-content--fixed">
+    <section>
+      <div class="container">
+        <h1>Users Page</h1>
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Age</th>
+              <th>Gender</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="user in users" :key="user.id">
+              <td>
+                {{ user.name }}
+              </td>
+              <td>
+                {{ user.age }}
+              </td>
+              <td>
+                {{ user.gender }}
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: "Users",
+  data() {
+    return {
+      users: [
+        { id: 1, name: "Maks", age: 44, gender: "M" },
+        { id: 2, name: "Vita", age: 42, gender: "G" },
+      ],
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+</style>
