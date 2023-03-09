7f30a61a455c590ba449384fc3524dc2a0626087
Maksim Zawadzki <zawadzki2778@gmail.com>
2023-03-09 15:37:32 +0300

    created vue-cli for CMR project

--- src/pages/Users.vue
+++ src/pages/Users.vue
@@ -2,7 +2,6 @@
   <div class="wrapper-content wrapper-content--fixed">
     <section>
       <div class="container">
-        <h1>Users Page</h1>
         <table>
           <thead>
             <tr>
